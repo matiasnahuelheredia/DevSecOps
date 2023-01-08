2f68435377c15bd52d4aab128e8891bdee07e996
router <router@localhost.localdomain>
2023-01-08 19:29:49 -0300

    change path of producer

--- src/main/java/com/matiasheredia/JavaTest/model/UseCases/MessageSender.java
+++
@@ -1,7 +0,0 @@
-package com.matiasheredia.JavaTest.model.UseCases;
-
-import com.matiasheredia.JavaTest.model.Entities.Message;
-
-public interface MessageSender {
-    public void sendAsyncMessage(Message message);
-}
